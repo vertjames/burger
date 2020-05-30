d0412e04523112ba66b930e7c0b6131fc298797e
vertjames <ianandrewkirby@gmail.com>
2020-05-29 21:00:39 -0700

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,4 @@
 const mysql = require('mysql2')
 
-module.exports = mysql.createConnection('mysql://root:rootroot@localhost:3306/burgers_db')
+// module.exports = mysql.createConnection('mysql://root:rootroot@localhost:3306/burgers_db')
+module.exports = mysql.createConnection(process.env.JAWSDB_URL || process.env.LOCAL_URL)
